d59ac40e996f874a211b0dcf2934fdf1cea743c3
hinokeita <hino@sackle.co.jp>
2019-01-22 01:07:19 +0000

    add:ruby版追加

--- stringSplosion/test1.rb
+++ stringSplosion/test1.rb
@@ -1,7 +1,7 @@
 def string_splosion(str)
   str.split("")
-    .map
-    .with_index{|v,i| str[0..i]}
-    .join
+     .map
+     .with_index{|v,i| str[0..i]}
+     .join
 end
 puts string_splosion("Code")
